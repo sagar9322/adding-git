abd5e21afa0d560f5533c84f395df9a26134b33c
sagar9322 <sagarkasundra223@gamil.com>
2023-05-08 12:45:25 +0530

    listFontBolded

--- domjs.js
+++ domjs.js
@@ -5,3 +5,8 @@ headerTitle.style.borderBottom = 'solid 3px black';
 var item = document.getElementsByClassName('list-group-item');
 item[1].style.fontWeight = "bold";
 item[1].style.color = "green";
+item[2].style.background = "green";
+
+for(let i = 0; i<item.length; i++){
+    item[i].style.fontWeight = "bold";
+}
