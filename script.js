c8c6dd5ee4b9fa2c4382364dfc4674cdfe7bcb8e
sagar9322 <sagarkasundra223@gmail.com>
2023-05-08 11:03:48 +0530

    first-comit

---
+++ script.js
@@ -0,0 +1,5 @@
+const ul = document.querySelector('.item');
+
+ul.firstElementChild.textContent = 'Hello';
+ul.firstElementChild.style.background = 'green';
+ul.children[1].style.background = 'yellow';
