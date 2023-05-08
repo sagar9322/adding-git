2ecf42deddcac2cee9849cca4ae8a138546fd815
sagar9322 <sagarkasundra223@gamil.com>
2023-05-08 14:28:49 +0530

    getelementbytagname

--- domjs.js
+++ domjs.js
@@ -1,12 +1,21 @@
 var headerTitle = document.getElementById('header-title');
 headerTitle.textContent = 'Hello';
 headerTitle.style.borderBottom = 'solid 3px black';
-
-var item = document.getElementsByClassName('list-group-item');
+//getElementsByClassName
+/*var item = document.getElementsByClassName('list-group-item');
 item[1].style.fontWeight = "bold";
 item[1].style.color = "green";
 item[2].style.background = "green";
 
 for(let i = 0; i<item.length; i++){
     item[i].style.fontWeight = "bold";
+}*/
+//GetElementByTagName
+var li = document.getElementsByTagName('li');
+li[1].style.fontWeight = "bold";
+li[1].style.color = "green";
+li[2].style.background = "green";
+
+for(let i = 0; i<li.length; i++){
+    li[i].style.fontWeight = "bold";
 }
