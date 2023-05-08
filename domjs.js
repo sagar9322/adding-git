bd148f0b033547893a8f8003740594e75883ca2d
sagar9322 <sagarkasundra223@gamil.com>
2023-05-08 15:23:09 +0530

    quearySelectorAll

--- domjs.js
+++ domjs.js
@@ -11,11 +11,24 @@ for(let i = 0; i<item.length; i++){
     item[i].style.fontWeight = "bold";
 }*/
 //GetElementByTagName
-var li = document.getElementsByTagName('li');
+/*var li = document.getElementsByTagName('li');
 li[1].style.fontWeight = "bold";
 li[1].style.color = "green";
 li[2].style.background = "green";
 
 for(let i = 0; i<li.length; i++){
     li[i].style.fontWeight = "bold";
+}*/
+
+var li = document.getElementsByTagName('li');
+li[1].style.background = "lightgreen";
+li[2].remove();
+
+let list = document.querySelectorAll('li');
+list[1].style.color = 'green';
+
+let odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(let i = 0; i<odd.length; i++){
+    odd[i].style.background = 'green';
 }
