be2b5c9da5e8d7597f7ce913bf64395824bf1980
sagar9322 <sagarkasundra223@gamil.com>
2023-05-08 23:16:50 +0530

    addDescription

--- itemLister.js
+++ itemLister.js
@@ -15,13 +15,14 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var itemDescription = document.getElementById('description').value;
 
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(`${newItem} ${itemDescription}`));
 
   // Create del button element
   var deleteBtn = document.createElement('button');
@@ -54,3 +55,19 @@ function removeItem(e){
   }
 }
 
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
