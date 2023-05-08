37c766e887d18780fc1053a76d2df565d7c4bf54
sagar9322 <sagarkasundra223@gamil.com>
2023-05-08 12:23:00 +0530

    domproject

---
+++ domjs.js
@@ -0,0 +1,7 @@
+var headerTitle = document.getElementById('header-title');
+headerTitle.textContent = 'Hello';
+headerTitle.style.borderBottom = 'solid 3px black';
+
+var item = document.getElementsByClassName('list-group-item');
+item[1].style.fontWeight = "bold";
+item[1].style.color = "green";
